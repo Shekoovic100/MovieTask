69e60edd701b48bca16ccce0d96abb7913ec8116
Sherif Samy <sherif.samy@orchtech.com>
2025-06-02 22:18:07 +0300

    Initial Commit

---
+++ MovieTask/MovieTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTaskApp.swift
+//  MovieTask
+//
+//  Created by sherif on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
