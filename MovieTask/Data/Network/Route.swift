9fbfc6968abd2a681993c3c6c1c0024a7cf0df2d
Sherif Samy <sherif.saamy@gmail.com>
2025-06-03 11:43:45 +0300

    merge details screen fetaure into main

--- MovieTask/Data/Network/Route.swift
+++ MovieTask/Data/Network/Route.swift
@@ -11,7 +11,7 @@ enum Route {
     
     static let BaseURL = "https://api.themoviedb.org/3/"
     private var getAPIKey: String {
-         let apiKey = "a5f1b980a72b26fba552d7fdb96a1390"
+         let apiKey = ""
         return apiKey
     }
     case upComingMovies(Int)
